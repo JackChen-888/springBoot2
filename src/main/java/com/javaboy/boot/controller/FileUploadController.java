c93e271848ed9006eebf85c5d2bbfc6e64e72e41
liuchen525@126.com <liuchen525@126.com>
2021-01-22 13:49:48 +0800

    文件上传

--- src/main/java/com/javaboy/boot/controller/FileUploadController.java
+++ src/main/java/com/javaboy/boot/controller/FileUploadController.java
@@ -1,15 +1,13 @@
 package com.javaboy.boot.controller;
 
+import com.documents4j.api.DocumentType;
+import com.documents4j.api.IConverter;
+import com.documents4j.job.LocalConverter;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.UUID;
+import java.io.*;
 
 /**
  * @author Chen
@@ -21,23 +19,27 @@ public class FileUploadController {
 
     @PostMapping("/upload")
 
-    public String upload(MultipartFile file, HttpServletRequest request) {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("/yyyy/MM/dd/");
-        String format = simpleDateFormat.format(new Date());
-        String s = request.getServletContext().getRealPath("/img") + format;
-        File folder = new File("Y:\\1");
+    public String upload(MultipartFile file) {
+        File folder = new File(File.separator + "testupload");
         if (!folder.exists()) {
             folder.mkdirs();
         }
-        String old = file.getOriginalFilename();
-        String name = UUID.randomUUID().toString() + old.substring(old.lastIndexOf("."));
+        String name = file.getOriginalFilename();
         try {
             file.transferTo(new File(folder, name));
-            String url = request.getScheme() + ("://") + request.getServerName() + ":" + request.getServerPort() + "/img" + format + name;
-            return url;
+//            File inputWord = new File("L:\\sss\\" + name);
+            File inputWord = new File(File.separator + "testupload" + File.separator + name);
+//            File outputFile = new File("L:\\sss\\Test_out2.pdf");
+            File outputFile = new File(File.separator + "testupload" + File.separator + "Test_out2.pdf");
+            InputStream docxInputStream = new FileInputStream(inputWord);
+            OutputStream outputStream = new FileOutputStream(outputFile);
+            IConverter converter = LocalConverter.builder().build();
+            converter.convert(docxInputStream).as(DocumentType.DOCX).to(outputStream).as(DocumentType.PDF).execute();
+            outputStream.close();
+            System.out.println("success");
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return null;
+        return "2G";
     }
 }

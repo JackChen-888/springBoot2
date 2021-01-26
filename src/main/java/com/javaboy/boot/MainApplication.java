5640f349b79a3c1f4939f199851b6b6ba329c1e4
liuchen525@126.com <liuchen525@126.com>
2021-01-26 09:51:19 +0800

    提交代码

--- src/main/java/com/javaboy/boot/MainApplication.java
+++ src/main/java/com/javaboy/boot/MainApplication.java
@@ -20,7 +20,7 @@ public class MainApplication {
         //2、查看容器里面的组件
         String[] names = run.getBeanDefinitionNames();
         for (String name : names) {
-            System.out.println(name);
+//            System.out.println(name);
         }
 
         //3、从容器中获取组件
@@ -29,23 +29,23 @@ public class MainApplication {
 
         Pet tom02 = run.getBean("tom", Pet.class);
 
-        System.out.println("组件：" + (tom01 == tom02));
+//        System.out.println("组件：" + (tom01 == tom02));
 
 
         //4、com.java.boot.config.MyConfig$$EnhancerBySpringCGLIB$$51f1e1ca@1654a892
         MyConfig bean = run.getBean(MyConfig.class);
-        System.out.println(bean);
+//        System.out.println(bean);
 
         //如果@Configuration(proxyBeanMethods = true)代理对象调用方法。SpringBoot总会检查这个组件是否在容器中有。
         //保持组件单实例
         User user = bean.user01();
         User user1 = bean.user01();
-        System.out.println(user == user1);
+//        System.out.println(user == user1);
 
 
         User user01 = run.getBean("user01", User.class);
         Pet tom = run.getBean("tom", Pet.class);
 
-        System.out.println("用户的宠物：" + (user01.getPet() == tom));
+//        System.out.println("用户的宠物：" + (user01.getPet() == tom));
     }
 }
